ebd84d82e71c917d4c5a79efeba1807daa63c79d
Vaibhav Tandon <VTandon@macmans-MacBook-Pro.local>
2020-03-16 21:09:50 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
